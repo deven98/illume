61adf428b42b94a20bab36ddb3368ba3435f343d
Deven Joshi <deven9852@gmail.com>
2021-12-08 19:38:18 +0530

    added flappy widget vertical example

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# illume 
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+A Widget-based game engine for Flutter 
 
 ## Features
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:illume/illume.dart';
 
@@ -29,37 +27,39 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+/// Quick demo for vertical screens
 class _MyHomePageState extends State<MyHomePage> {
   IllumeController gameController = IllumeController();
 
   FlappyWidget flappyWidget = FlappyWidget();
+  Wall wall = Wall(200, false);
+  Wall wall2 = Wall(400, true);
 
   @override
   void initState() {
     super.initState();
     gameController.startGame();
-    gameController.gameObjects.add(flappyWidget);
+    gameController.gameObjects.addAll([flappyWidget, wall, wall2]);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Illume Demo'),
-      ),
-      body: Stack(
-        children: [
-          gradient,
-          GestureDetector(
-            child: Illume(
+    return GestureDetector(
+      onTap: () {
+        flappyWidget.jump();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Illume Demo'),
+        ),
+        body: Stack(
+          children: [
+            gradient,
+            Illume(
               illumeController: gameController,
             ),
-            onTap: () {
-              print('tap');
-              flappyWidget.jump();
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -77,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class FlappyWidget extends GameObject {
   var velocity = 0.0;
-  var acceleration = 0.05;
+  var acceleration = 0.2;
 
   @override
   Widget build(BuildContext context) {
@@ -109,6 +109,52 @@ class FlappyWidget extends GameObject {
   }
 
   void jump() {
-    velocity = -10;
+    velocity = -5;
+  }
+}
+
+class Wall extends GameObject {
+  int initialDistance;
+  bool vert;
+
+  Wall(
+    this.initialDistance,
+    this.vert,
+  );
+
+  var velocity = -2.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: const Text('Demo'),
+    );
+  }
+
+  @override
+  void init() {
+    size = Vector2(50, gameSize[1] / 2);
+    alignment = GameObjectAlignment.center;
+    position = Vector2(
+      (gameSize[0] / 2) + initialDistance,
+      (vert ? 1 : 3) * gameSize[1] / 4,
+    );
+  }
+
+  @override
+  void onCollision(List<Collision> collisions) {
+    illumeController.stopGame();
+  }
+
+  @override
+  void onScreenSizeChange(Vector2 size) {}
+
+  @override
+  void update(Duration delta) {
+    position += Vector2(velocity, 0);
+    if (position[0] < -50) {
+      position[0] = gameSize[0] + 25;
+    }
   }
 }
